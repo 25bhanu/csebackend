dd933473c5d0d36517d71ad5f0944dd0718806f5
25bhanu <bhanupratap252002@gmail.com>
2025-02-11 10:15:53 +0530

    latest data

--- cse c/filesystem/filereadsync.js
+++ cse c/filesystem/filereadsync.js
@@ -1,3 +1,21 @@
 const fs = require("fs");
-const data = fs.readFileSync("./data.txt");
+// fs.writeFileSync("./data1.txt", "Hello ABES", "utf-8");
+// fs.writeFileSync("./data2.txt", "Hello ABESec" , "utf-8");
+// fs.appendFileSync("./data1.txt", "Students", "utf-8");
+
+// fs.renameSync("./data1.txt", "./data3.txt" , "utf-8");
+// fs.unlinkSync("./data3.txt" , "utf-8");
+const data = fs.readFileSync("./data.txt", "utf-8");
 console.log(data)
+if(data.match("H")){
+    console.log("File contians 'H'");
+    const newdata = data.replace("H", "ABES");
+    fs.writeFileSync("./data.txt", newdata, "utf-8");
+}
+
+
+
+
+// const data = fs.readFileSync("./data.txt","utf-8");
+// console.log(data.toString)
+// console.log(data)
